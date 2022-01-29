e769d4c3caacb566e10edd99239d0b2d4176883d
SheikhSahab01 <63442347+SheikhSahab01@users.noreply.github.com>
2022-01-29 17:39:47 +0530

    Add files via upload

---
+++ solution1.js
@@ -0,0 +1,7 @@
+var A = Number(prompt("Enter the first Number"));
+var B = Number(prompt("Enter the Second Number"));
+
+
+const result = (A * B);
+
+console.log(`The multiplication of ${A} and ${B} is ${result}`);
